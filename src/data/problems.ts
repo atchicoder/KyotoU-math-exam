889ff05b90fa33015bafd512fa9fe9f6a814803d
atchicoder <atchi_naga@yahoo.co.jp>
2025-01-22 14:18:10 +0900

    2022年第1問追加

--- src/data/problems.ts
+++ src/data/problems.ts
@@ -1,7 +1,8 @@
+import { 文系2022 } from "./文系/2022";
 import { 文系2023 } from "./文系/2023";
 import { 文系2024 } from "./文系/2024";
-
 // 理系データも同様にインポート
 // import { 理系2023 } from "./理系/2023";
 // import { 理系2024 } from "./理系/2024";
-export const problems = [...文系2023, ...文系2024];
+
+export const problems = [...文系2023, ...文系2024, ...文系2022];
