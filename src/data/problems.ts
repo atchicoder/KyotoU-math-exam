97af164d2941465238a533a304513b510e7f80fd
atchicoder <atchi_naga@yahoo.co.jp>
2025-01-22 15:29:56 +0900

    2022年第1問追加 Bunkeiに変更

--- src/data/problems.ts
+++ src/data/problems.ts
@@ -1,8 +1,8 @@
-import { 文系2022 } from "./文系/2022";
-import { 文系2023 } from "./文系/2023";
-import { 文系2024 } from "./文系/2024";
+import { Bunkei2022 } from "./Bunkei/2022";
+import { Bunkei2023 } from "./Bunkei/2023";
+import { Bunkei2024 } from "./Bunkei/2024";
 // 理系データも同様にインポート
 // import { 理系2023 } from "./理系/2023";
 // import { 理系2024 } from "./理系/2024";
 
-export const problems = [...文系2023, ...文系2024, ...文系2022];
+export const problems = [...Bunkei2023, ...Bunkei2024, ...Bunkei2022];
