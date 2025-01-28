317edfbf8abe2db57ada2739a24df229071a2e24
atchicoder <atchi_naga@yahoo.co.jp>
2025-01-28 21:24:31 +0900

    2021文系問1追加

--- src/data/problems.ts
+++ src/data/problems.ts
@@ -1,3 +1,4 @@
+import { Bunkei2021 } from "./Bunkei/2021";
 import { Bunkei2022 } from "./Bunkei/2022";
 import { Bunkei2023 } from "./Bunkei/2023";
 import { Bunkei2024 } from "./Bunkei/2024";
@@ -6,4 +7,4 @@ import { Rikei2003 } from "./Rikei/2003"
 // import { 理系2023 } from "./理系/2023";
 // import { 理系2024 } from "./理系/2024";
 
-export const problems = [...Bunkei2023, ...Bunkei2024, ...Bunkei2022, ...Rikei2003];
+export const problems = [...Bunkei2023, ...Bunkei2024, ...Bunkei2022, ...Bunkei2021, ...Rikei2003];
