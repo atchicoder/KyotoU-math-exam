bf589a95359a79778b66fa5d0cc04b2818962bda
atchicoder <atchi_naga@yahoo.co.jp>
2025-01-31 22:02:30 +0900

    2020年第1問、第2問追加

--- src/data/problems.ts
+++ src/data/problems.ts
@@ -1,3 +1,4 @@
+import { Bunkei2020 } from "./Bunkei/2020";
 import { Bunkei2021 } from "./Bunkei/2021";
 import { Bunkei2022 } from "./Bunkei/2022";
 import { Bunkei2023 } from "./Bunkei/2023";
@@ -7,4 +8,4 @@ import { Rikei2003 } from "./Rikei/2003"
 // import { 理系2023 } from "./理系/2023";
 // import { 理系2024 } from "./理系/2024";
 
-export const problems = [...Bunkei2023, ...Bunkei2024, ...Bunkei2022, ...Bunkei2021, ...Rikei2003];
+export const problems = [...Bunkei2023, ...Bunkei2024, ...Bunkei2022, ...Bunkei2021, ...Bunkei2020,...Rikei2003];
