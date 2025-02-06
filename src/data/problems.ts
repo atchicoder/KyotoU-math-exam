293f7b2efd91a9bcd9057d786801142fc21f9c1a
atchicoder <atchi_naga@yahoo.co.jp>
2025-02-06 21:58:50 +0900

    2024年理系第1問、第2問追加、不具合修正２

--- src/data/problems.ts
+++ src/data/problems.ts
@@ -3,8 +3,8 @@ import { Bunkei2021 } from "./Bunkei/2021";
 import { Bunkei2022 } from "./Bunkei/2022";
 import { Bunkei2023 } from "./Bunkei/2023";
 import { Bunkei2024 } from "./Bunkei/2024";
-import { Rikei2024 } from "./Rikei/2024"
-import { Rikei2003 } from "./Rikei/2003"
+import { Rikei2024 } from "./Rikei/2024";
+import { Rikei2003 } from "./Rikei/2003";
 // 理系データも同様にインポート
 // import { 理系2023 } from "./理系/2023";
 // import { 理系2024 } from "./理系/2024";
