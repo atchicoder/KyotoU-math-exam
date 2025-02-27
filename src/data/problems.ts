875febbfaf4a18b6efa323c137ca8a5c857de395
atchicoder <atchi_naga@yahoo.co.jp>
2025-02-27 16:56:18 +0900

    2025年文系第1問追加

--- src/data/problems.ts
+++ src/data/problems.ts
@@ -3,6 +3,7 @@ import { Bunkei2021 } from "./Bunkei/2021";
 import { Bunkei2022 } from "./Bunkei/2022";
 import { Bunkei2023 } from "./Bunkei/2023";
 import { Bunkei2024 } from "./Bunkei/2024";
+import { Bunkei2025 } from "./Bunkei/2025";
 import { Rikei2024 } from "./Rikei/2024";
 import { Rikei2003 } from "./Rikei/2003";
 import { Rikei2023 } from "./Rikei/2023";
@@ -12,4 +13,4 @@ import { Rikei2021 } from "./Rikei/2021";
 // import { 理系2023 } from "./理系/2023";
 // import { 理系2024 } from "./理系/2024";
 
-export const problems = [...Bunkei2023, ...Bunkei2024, ...Bunkei2022, ...Bunkei2021, ...Bunkei2020, ...Rikei2024, ...Rikei2003, ...Rikei2023, ...Rikei2022, ...Rikei2021];
+export const problems = [...Bunkei2025, ...Bunkei2023, ...Bunkei2024, ...Bunkei2022, ...Bunkei2021, ...Bunkei2020, ...Rikei2024, ...Rikei2003, ...Rikei2023, ...Rikei2022, ...Rikei2021];
